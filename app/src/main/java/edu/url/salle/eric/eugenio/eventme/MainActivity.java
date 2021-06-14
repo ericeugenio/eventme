fa92c97c82e555f14568a457061f200bfd994b3f
eric.eugenio <eric.eugenio@students.salle.url.edu>
2021-06-14 18:17:39 +0200

    splash screen done

--- app/src/main/java/edu/url/salle/eric/eugenio/eventme/MainActivity.java
+++ app/src/main/java/edu/url/salle/eric/eugenio/eventme/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -31,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.add(R.id.nav_host_fragment, HomeFragment.getInstance());
         mFragmentTransaction.commit();
+    }
+
+    public static Intent newIntent(Context packageContext) {
+        return new Intent(packageContext, MainActivity.class);
     }
 
     public void onClickChangeView(View view) {
--- app/src/main/java/edu/url/salle/eric/eugenio/eventme/SplashActivity.java
+++ app/src/main/java/edu/url/salle/eric/eugenio/eventme/SplashActivity.java
@@ -2,13 +2,34 @@ package edu.url.salle.eric.eugenio.eventme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    // Duration in ms
+    private final static int SPLASH_DURATION = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = MainActivity.newIntent(SplashActivity.this);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
     }
 }
