b3ea554cd0a01d0c1efa18fba9a6584f4d96ab03
eric.eugenio <eric.eugenio@students.salle.url.edu>
2021-06-16 19:02:08 +0200

    stories started

---
+++ app/src/main/java/edu/url/salle/eric/eugenio/eventme/StoriesActivity.java
@@ -0,0 +1,14 @@
+package edu.url.salle.eric.eugenio.eventme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories);
+    }
+}
